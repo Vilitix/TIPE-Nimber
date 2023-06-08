e15a147d69423431948e312fc10fec6dccde2ce9
Arthur Saltel <arthsaltel@gmail.com>
2023-06-08 20:59:15 +0200

    ajout de dots and box nouveau projet de jeu

---
+++ dots_and_boxes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+int** board(int x, int y){
+    int** res = malloc (sizeof(int*)*y);
+    for (int i = 0; i<x;i++){
+    int* ligne = malloc(sizeof(int)*x);
+    res[i] = ligne 
+    }
+}
